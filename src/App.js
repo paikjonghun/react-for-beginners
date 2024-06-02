1efe259e136a32c52cba63b4e4e617d9555fbf17
백종훈 <hun3072@gmail.com>
2024-06-02 18:24:12 +0900

    App.js 라우터 베이스 경로 수정

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/movie/:id">
           <Detail />
